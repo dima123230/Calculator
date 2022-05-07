cf51ecd65b6d118f0891e10977fba65e1855c3d0
Nevedrov Dmitriy <nevedrovdima@mail.ru>
2022-05-07 15:23:50 +0300

    Calculator with vectors.

--- Calculator/main.c
+++ Calculator/main.c
@@ -5,11 +5,14 @@
  Version     :
  Copyright   : Your copyright notice
  Description : Calculator with 6 operations with real numbers: (+),(-),(*),(/),(^),(!)
+			   and 3 operations with vectors (+),(-),(*).
  ============================================================================
  */
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(void)
 
@@ -25,17 +28,56 @@ int main(void)
 	puts("4. / (divides the first number by the second");
 	puts("5. ^ (raises the first number to the power of the second)");
 	puts("6. ! (Finds the factorial of a number)");
+	puts("7. v (uses a code branch with vector operations: (+),(-),(*).)");
 
 	long int f, g;    // Переменные целых чисел для операции "возведение в степень ^".
 	long long int j, res = 1;  // Переменные целых чисел для операции "факториал числа !".
 	double a, b;      // Переменные вещественных чисел для операций: "плюс, минус, деление и умножение".
+	double* A, * B;  // Переменные для операций с векторами.
+	double resv;
 	char c, r;       // Переменные для символов, первая для выбора операции, вторая для повторения работы калькулятора.
+	char op;         // Операция с векторами.
+	int size;
 	do  // Цикл если пользователь захочет продолжить работу с программой
 	{
-		printf("Enter one of the provided operations: (+),(-),(*),(/),(^),(!): ");
+		printf("Enter one of the provided operations: (+),(-),(*),(/),(^),(!) (v): ");
 		scanf(" %c", &c); // Выбор одной из операций.
 		switch (c)
 		{
+		case 'v': // Операции с векторами.
+			printf("Enter the size of the vectors: ");
+			scanf("%i", &size);
+			A = malloc(size * sizeof(int));
+			B = malloc(size * sizeof(int));
+			printf("Enter the coordinates of the first vector: ");
+			for (int i = 0; i < size; i++) scanf("%lf", &A[i]);
+			printf("Enter the coordinates of the second vector: ");
+			for (int i = 0; i < size; i++) scanf("%lf", &B[i]);
+			printf("Enter one of the provided operations for vectors: (+),(-),(*): ");
+			scanf(" %c", &op);
+			switch (op)
+			{
+			case '+': // Операция сложения с векторами.
+				printf("Addition of vectors: ");
+				for (int i = 0; i < size; i++) printf("%lf ", A[i] + B[i]);
+				printf("\n");
+				break;
+			case '-': // Операция вычитания с векторами.
+				printf("Вычитание векторов: ");
+				for (int i = 0; i < size; i++) printf("%lf ", A[i] - B[i]);
+				printf("\n");
+				break;
+			case '*': // Операция скалярного произведения с векторами.
+				printf("Скалярное произведение векторов: ");
+				for (int i = 0; i < size; i++) printf("%lf ", A[i] * B[i]);
+				printf("\n");
+				break;
+			default: // В случае неверной выбранной операции.
+				printf("Неправильно выбрана операция!");
+			}
+			free(A); // Освобождение использованной памяти.
+			free(B);
+			break;
 		case '+': // Операция сложения двух чисел.
 			printf("Enter the first number:");
 			scanf("%lf", &a);
